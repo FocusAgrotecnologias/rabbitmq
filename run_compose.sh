c92df342cc1d4674e0f91e1086ee1e25cc70534b
Rodrigo Miranda <rodrigo.mdc2010@gmail.com>
2026-01-30 23:30:55 -0300

    Ajuste no sh para buildar corretamente.

--- run_compose.sh
+++ run_compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #curl -u admin:pD265Yf9xPw0 http://localhost:15672/api/definitions > rabbitmq/definitions.json
-docker-compose down
-docker-compose build --no-cache
-docker-compose up
+docker compose down
+docker compose build --no-cache
+docker compose up -d
